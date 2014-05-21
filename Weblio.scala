b780f53f9d9f11f799474d1d24c68ff69ed691f8
Kento Masui <nameissoap@gmail.com>
2014-05-22 01:44:47 +0900

    handled errors

--- Weblio.scala
+++ Weblio.scala
@@ -1,41 +1,57 @@
 import java.io.PrintWriter
 import org.jsoup.Jsoup
-import org.jsoup.nodes.{Node, Element}
-import org.jsoup.select.Elements
+import org.jsoup.nodes.Element
 import scala.collection.JavaConversions._
+import scala.util.Try
 import scalaz._
 import Scalaz._
-object Weblio{
-  def definition(article:Element)={
-    val elems = article.getElementsByTag("div").get(1)
-    elems.getElementsByTag("div").map(_.text()).mkString("\n")
+
+object Weblio {
+  val definition = (article: Element) => Try {
+    val result = article.getElementsByTag("div").get(1).text()
+    if (result.isEmpty) throw new RuntimeException("article was empty") else result
   }
-  def articles(vocab:String) = {
-    //TODO scala 2.10/2.11にしてTryを使う
-    val doc = Jsoup.connect("http://ejje.weblio.jp/content/"+vocab).get
+  val articles = (word: String) => Try {
+    val doc = Jsoup.connect("http://ejje.weblio.jp/content/" + word).get
     val body = doc.body()
-    val base = body.getElementsByAttributeValueMatching("id","base").first()
-    val main = base.getElementsByAttributeValueMatching("id","main").first()
+    val base = body.getElementsByAttributeValueMatching("id", "base").first()
+    val main = base.getElementsByAttributeValueMatching("id", "main").first()
     main.getElementsByClass("kiji")
   }
 }
-object Anki{
-  def convert(words:Seq[(String,String)]):String={
-    words.map{
-      case(word,definition)=>word + ";\"" + definition.replaceAll(";","::").replaceAll("\"","''")+"\""
+
+object Anki {
+  val convert = (words: Seq[(String, String)]) => {
+    words.map {
+      case (word, definition) => word + ";\"" + definition.replaceAll(";", "::").replaceAll("\"", "''") + "\""
     }.mkString("\n")
   }
 }
-object Main{
+
+object Main {
   import Weblio._
   import Anki._
-  def main(args: Array[String])={
+  def main(args: Array[String]) = {
     printWords(args)
   }
-  def printWords = convertWords _ andThen print
-  def convertWords(words:Seq[String]) = words.par.map{
-    word => word->createCard(word)
-  }.toBuffer |> convert
-  def createCard(word:String) = articles(word).map(definition).mkString("\n")
-  def export(out:String,value:String) = new PrintWriter(out) <| (_.print(value)) |> (_.close)
+
+  val createCard = articles andThen (_.flatMap(_.map(definition).collect {
+    case scala.util.Success(s) => s
+  }.mkString("\n") match {
+    case "" => Try(throw new RuntimeException("no definition"))
+    case s => Try(s)
+  }))
+  val convertWords = (words: Seq[String]) => words.par.map {
+    word => word -> createCard(word)
+  }.partition(_._2.isSuccess)
+  val printWords = convertWords andThen {
+    case (successes, failures) => {
+      failures.foreach(System.err.println)
+
+      successes.collect {
+        case (word, util.Success(defined)) => word -> defined
+      }.toBuffer |> convert |> print
+    }
+  }
+  val export = (out: String, value: String) => new PrintWriter(out) <| (_.print(value)) |> (_.close)
 }
